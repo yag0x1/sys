7579db671dbe4b23593a11c30593752d0b81f467
Yunior <52602415+yag0x1@users.noreply.github.com>
2023-09-10 09:56:39 -0300

    Update initial_config.sh

--- initial_config.sh
+++ initial_config.sh
@@ -1,32 +1,17 @@
 #!/bin/bash
+sudo apt-get update
+sudo apt-get upgrade
 
 sudo rm -rf /var/lib/apt/lists/lock
 sudo rm /var/lib/dpkg/lock
 sudo rm /var/cache/apt/archives/lock
-
-sudo apt-get update
-sudo dpkg –configure -a
-sudo apt-get -f install
-
 sudo rm /var/lib/dpkg/lock-frontend
-sudo dpkg --configure -a
-sudo apt update
-
-sudo apt-get update # Update
-
-sudo apt install snapd -y
-sudo apt-get install gnome-software-plugin-snap -y
-sudo apt install flatpak -y
-sudo apt install gnome-software-plugin-flatpak -y
-
 
 sudo apt install btop -y #Resource monitor
 sudo apt install zsh -y #installing the zsh
 sudo apt install tmux -y #split terminal and save sections
 
 sudo apt install ranger -y #Files Manager on terminal
-
-sudo apt install speedtest-cli -y  #test internet velocity
 
 sudo apt autoremove -y
 
