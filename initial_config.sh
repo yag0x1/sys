c66aedbd42bd412492bcc99a0710ddbc750eb3f2
Yunior <52602415+yag0x1@users.noreply.github.com>
2023-09-10 10:39:00 -0300

    Update initial_config.sh

--- initial_config.sh
+++ initial_config.sh
@@ -8,9 +8,11 @@ sudo rm /var/cache/apt/archives/lock
 sudo rm /var/lib/dpkg/lock-frontend
 
 sudo apt install btop -y #Resource monitor
-sudo apt install zsh -y #installing the zsh
 sudo apt install tmux -y #split terminal and save sections
 sudo apt install ranger -y #Files Manager on terminal
+
+sudo apt install zsh -y #installing the zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 sudo apt autoremove -y
 
